65cec010b19ad6e0ba4f54a54aac1732b36676cd
Jeremy Wolff <jwolff@dsdmail.net>
2018-04-27 15:11:24 -0600

    Cars Controlled by 2 controllers. Add more images via WebGLTemplates/Volplane/img

---
+++ Assets/CameraControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+    [SerializeField] bool freezeCameraRotation;
+    Quaternion origRotation;
+
+	// Use this for initialization
+	void Start () {
+        origRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (freezeCameraRotation) {
+            transform.rotation = origRotation;
+        }
+	}
+}
